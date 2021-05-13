a8af268bd25ffcc201dbdc2c75dd392326e66c04
Aditya <adityarao1999.ar@gmail.com>
2021-05-13 09:55:14 +0530

    Sign Up

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,6 @@
+// get user model
+const User = require('../models/user');
+
 module.exports.profile = function(req, res) {
     return res.render('user_profile', {
         title: "User Profile"
@@ -21,6 +24,26 @@ module.exports.signIn = function(req, res) {
 // get the sign up data
 module.exports.create = function(req, res) {
 
+
+    if (req.body.password !== req.body.confirm_password) {
+        // console.log(req.body);
+        return res.redirect('back');
+    }
+
+    User.findOne({ email: req.body.email }, function(err, user) {
+        if (err) { console.log('error in finding user in signing up'); return; }
+
+        if (!user) {
+            User.create(req.body, function(err, user) {
+                if (err) { console.log('error in creating user While signing up'); return; }
+
+                return res.redirect('/users/sign-in')
+            })
+        } else {
+            return res.redirect('back');
+        }
+
+    });
 }
 
 // sign in and create a session for the user
