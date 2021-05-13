8f29524bdb758a8d69cad7ec216d2c423d0322dd
Aditya <adityarao1999.ar@gmail.com>
2021-05-13 08:04:32 +0530

    altered cookies

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,6 +3,7 @@ module.exports.home = function(req, res) {
     console.log(req.cookies);
     // change cookies
     res.cookie('user_id', 25);
+    res.cookie('new_id', 4678);
 
     // return res.end('<h1>Express is up for codeial</h1>');
     return res.render('home', {
