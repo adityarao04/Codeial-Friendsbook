00e68cbd8bda9ed703bff932b66e2729f2ed7a90
Aditya <adityarao1999.ar@gmail.com>
2021-06-08 22:20:57 +0530

    Nested Population :: Display Comment and related user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,14 +1,28 @@
+const Post = require('../models/post')
+
 module.exports.home = function(req, res) {
     // check cookies
-    console.log(req.cookies);
+    // console.log(req.cookies);
     // change cookies
-    res.cookie('user_id', 25);
+    // res.cookie('user_id', 25);
     // res.cookie('new_id', 4678);
 
     // return res.end('<h1>Express is up for codeial</h1>');
-    return res.render('home', {
-        title: "Home"
-    });
+    // Post.find({}, function(err, posts) {
+    //     return res.render('home', {
+    //         title: " Friendsbook | Home",
+    //         posts: posts
+    //     });
+    // })
+
+    // populate the user of each post
+    Post.find({}).populate('user').exec(function(err, posts) {
+        return res.render('home', {
+            title: " Friendsbook | Home",
+            posts: posts
+        });
+    })
+
 
 }
 
