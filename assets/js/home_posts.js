e82571a3eb49150ff327d96a6f7368c7592aa991
Aditya <adityarao1999.ar@gmail.com>
2022-02-13 22:56:03 +0530

    code

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -1,3 +1,92 @@
 {
+    // method to submit form data for new post using AJAX
+    let createpost = function() {
 
+        let newPostFrom = $('#new-post-form');
+
+        newPostFrom.submit(function(e) {
+            e.preventDefault();
+
+
+            $.ajax({
+                type: 'POST',
+                url: '/posts/create',
+                data: newPostFrom.serialize(),
+                success: function(data) {
+                    console.log(data);
+                    let newPost = newPostDom(data.data.post);
+                    $('#posts-list-container>ul').prepend(newPost);
+                    deletePost($('.delete-post-button', newPost));
+                },
+                error: function(error) {
+                    console.log(error.reponseText);
+                }
+            })
+        })
+    }
+
+
+    // method to create a post in DOM
+    let newPostDom = function(post) {
+        return $(`<li id='post-${post._id}'>
+        <p>
+            
+    
+    
+                <small>
+            <a class='delete-post-button' href="/posts/destroy/${post._id}">X</a>
+             </small>
+               
+             ${post.content}
+                        <br>
+                        <small>
+                        ${post.user.name}
+            </small>
+                        <small>
+        
+          </small>
+        </p>
+        <div class="post-comments">
+            
+                <form action="/comments/create" method="POST" id="comments-form">
+                    <input type="text" name="content" autocomplete="off" placeholder="Type Here to add comment..." required>
+                    <input type="hidden" autocomplete="off" name="post" value="${post._id}">
+                    <input type="submit" value="Add Comment">
+                </form>
+    
+    
+                
+    
+                    <div class="post-comments-list">
+                        <ul id="post-comments-${post._id}">
+                           
+    
+                        </ul>
+                    </div>
+        </div>
+    </li>`)
+    }
+
+
+
+    // method to delete a post from DOM
+    let deletePost = function(deleteLink) {
+        $(deleteLink).click(function(e) {
+            e,
+            preventDefault();
+
+            $.ajax({
+                type: 'get',
+                url: $(deleteLink).prop('href'),
+                success: function(data) {
+                    $(`#post-${data.post_id}`).remove()
+                },
+                error: function(error) {
+                    console.log(error.reponseText);
+                }
+            })
+        })
+    }
+
+    createpost();
 }
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -89,5 +89,5 @@ module.exports.destroySession = function(req, res) {
     req.logout();
     req.flash('success', 'You have Logged Out');
 
-    return res.redirect('/')
+    return res.redirect('/users/sign-in');
 }
