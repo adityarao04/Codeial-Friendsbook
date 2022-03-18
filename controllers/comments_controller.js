0977ca870d339680a0fcec3d93ca9cf4f7e58e7a
Aditya Rao <adityarao1999.ar@gmail.com>
2022-03-18 23:18:11 +0530

    adding comment

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -10,7 +10,7 @@ module.exports.create = async function(req, res) {
 
     try {
         let post = await Post.findById(req.body.post);
-
+        // sentiment analysis API
         let postData = `http://127.0.0.1:5000/?q=\'${req.body.content}\'`
         var options = {
             'method': 'GET',
